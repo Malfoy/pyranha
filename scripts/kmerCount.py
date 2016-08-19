94da4fb42dce467e2a3624b1371188be50c1931e
kamimrcht <camille.marchet@inria.fr>
2016-08-19 21:07:18 +0200

    modified script to use KMC binaries from /bin

--- scripts/kmerCount.py
+++ scripts/kmerCount.py
@@ -5,6 +5,9 @@ import subprocess,sys,struct,os,time,shutil
 if len(sys.argv) < 5:
 	sys.exit( 'Usage : [Read file] [Reference file] [Kmer size] [Output  file]    NB: fasta file please or modify the script :)' )
 
+scriptDir = os.path.dirname(os.path.realpath(__file__))
+binDir = scriptDir + "/../bin/"
+
 debut=time.time()
 
 readFile=sys.argv[1]
@@ -15,16 +18,16 @@ outFile=sys.argv[4]
 subprocess.call(["mkdir", "readFolderKMC", "referenceFolderKMC"])
 
 print "Kmercount of the reference file"
-subprocess.call(["kmc", "-fm", "-k"+kmerSize, "-ci1", refFile, "reference", "referenceFolderKMC"])
+subprocess.call([binDir + "kmc", "-fm", "-k"+kmerSize, "-ci1", refFile, "reference", "referenceFolderKMC"])
 
 print "Kmercount of the read file"
-subprocess.call(["kmc", "-fa", "-k"+kmerSize, "-ci1", readFile, "read", "readFolderKMC"])
+subprocess.call([binDir + "kmc", "-fa", "-k"+kmerSize, "-ci1", readFile, "read", "readFolderKMC"])
 
 print "Merging"
-subprocess.call(["kmc_tools", "intersect",   "reference", "read","-ci1", "intersection"])
+subprocess.call([binDir + "kmc_tools", "intersect",   "reference", "read","-ci1", "intersection"])
 
 print "Dumping"
-subprocess.call(["kmc_dump", "intersection", outFile])
+subprocess.call([binDir + "kmc_dump", "intersection", outFile])
 
 print "CLeaning"
 subprocess.call(["rm", 'reference.kmc_pre', 'reference.kmc_suf', 'read.kmc_suf', 'read.kmc_pre', 'intersection.kmc_pre', 'intersection.kmc_suf'])
